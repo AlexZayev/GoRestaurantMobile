633484c7559205c774d15970dbd1480df127aa3e
Alex Castro <castro.aco@gmail.com>
2020-10-08 19:49:34 -0300

    desafio finalizado

--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -33,6 +33,14 @@ const Orders: React.FC = () => {
   useEffect(() => {
     async function loadOrders(): Promise<void> {
       // Load orders from API
+      await api.get('/orders').then(response => {
+        setOrders(
+          response.data.map((order: Food) => ({
+            ...order,
+            formattedPrice: formatValue(order.price),
+          })),
+        );
+      });
     }
 
     loadOrders();
